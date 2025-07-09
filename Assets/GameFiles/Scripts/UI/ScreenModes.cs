a565653fc3ffcb8d6aae3aaf31266a1dfd8659b7
Kamil <kamiloso.skoposo@gmail.com>
2025-07-09 11:37:08 +0200

    Aspect ratio & F11

---
+++ Assets/GameFiles/Scripts/UI/ScreenModes.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Larnix.UI
+{
+    public class ScreenModes : MonoBehaviour
+    {
+        private static bool initialized = false;
+
+        private void Start()
+        {
+            if (!initialized)
+            {
+                initialized = true;
+                SetFullScreen(true);
+            }
+        }
+
+        private void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.F11))
+                SetFullScreen(!Screen.fullScreen);
+        }
+
+        private void SetFullScreen(bool full)
+        {
+            if (full)
+            {
+                int nativeWidth = Screen.currentResolution.width;
+                int nativeHeight = Screen.currentResolution.height;
+                Screen.SetResolution(nativeWidth, nativeHeight, true);
+            }
+            else
+            {
+                Screen.SetResolution(1280, 720, false);
+            }
+        }
+    }
+}
